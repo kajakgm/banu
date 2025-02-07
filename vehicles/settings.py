e4a863064ae1c7c9cfc90102e98a2b139a3a0dd8
unknown <kajakgm@gmail.com>
2025-02-07 16:58:30 +0530

    first commit

--- vehicles/settings.py
+++ vehicles/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 import os
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -127,6 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT=os.path.join(BASE_DIR,'static')
 
 # managing media
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
